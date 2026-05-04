4a83a3938ecf627fe94b94ad78868c83016df646
LARAND <andrejlarin88064@gmail.com>
2026-05-04 15:53:31 +0300

    + UDP

--- Управление EPOS2/Application.py
+++ Управление EPOS2/Application.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
+"""
+'SPEED' - Задать скорость
+'STOP' - Отключить силовой элемент
+'START' - Включить силовой элемент
+'HELP' - Вывксти справку по командам
+"""
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import socket
+
+UDP_IP = "192.168.1.10" # Адерс STM32F767ZI
+UDP_PORT = 5000
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+print(f"Maxon Motor. Подключение к {UDP_IP}:{UDP_PORT}")
+print("Введите 'HELP' для справки. Для выхода нажмите Ctrl+C")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+try:
+    while True:
+        str_cmd = input("Ожидание ввода: ").strip().upper()
 
+        if str_cmd == "SPEED":
+            speed_str = input("Введите скорость (RPM): ").strip()
+            try:
+                speed_int = int(speed_str)
+                # Отправляем строку в кодировке UTF-8
+                sock.sendto(speed_str.encode('utf-8'), (UDP_IP, UDP_PORT))
+                print(f"Отправлена команда: {speed_int} RPM")
+            except ValueError:
+                print("Ошибка: Введите целое число!")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        elif str_cmd == "STOP":
+            sock.sendto(b"CMD_STOP", (UDP_IP, UDP_PORT))
+            print("Силовой каскад выключен!\n")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        elif str_cmd == "START":
+            sock.sendto(b"CMD_START", (UDP_IP, UDP_PORT))
+            print("Силовой каскад включен!\n")
+
+        elif str_cmd == "HELP":
+            print("\nСПРАВКА ПО КОМАНДАМ\n")
+            print(" SPEED  - Задать скорость мотора (потребует ввод числа)")
+            print(" STOP   - Отключить силовой каскад (вал станет свободным, Controlword = 0x07)")
+            print(" START  - Включить силовой каскад (вал станет упругим, Controlword = 0x0F)")
+            print(" HELP   - Вывести это меню\n")
+        elif str_cmd == "":
+            pass
+        else:
+            print("Введена некорректная команда! Введите HELP для справки.\n")
+
+except KeyboardInterrupt:
+    sock.sendto(b"0", (UDP_IP, UDP_PORT))
+    sock.sendto(b"CMD_STOP", (UDP_IP, UDP_PORT))
+    print("\nМотор остановлен. Выход.")
+
